dca6af8d23bdcadb91c049c9832a31a812e71b6d
xpaww <verdo.dev@gmail.com>
2022-02-07 10:23:28 +0700

    add-expires-token

--- src/tokenize/tokenManager.js
+++ src/tokenize/tokenManager.js
@@ -3,7 +3,7 @@ const Jwt = require('jsonwebtoken');
 const InvariantError = require('../error/InvariantError');
 
 const TokenManager = {
-  generateAccessToken: (payload) => Jwt.sign(payload, process.env.ACCESS_TOKEN_KEY),
+  generateAccessToken: (payload) => Jwt.sign(payload, process.env.ACCESS_TOKEN_KEY, { expiresIn: 3000 }),
   generateRefreshToken: (payload) => Jwt.sign(payload, process.env.REFRESH_TOKEN_KEY),
   verifyRefreshToken: (refreshToken) => {
     try {
